da12a0e0617cf555768409e23828ed537c59379f
Benjamin Fritsch <ben@lomography.com>
2011-05-04 15:11:52 +0200

    Initial commit

---
+++ LomoPhotos/Photo.h
@@ -0,0 +1,16 @@
+//
+//  Photo.h
+//  LomoPhotos
+//
+//  Created by ben on 03.05.11.
+//  Copyright 2011 innomind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Photo : NSObject {
+    
+}
+
+@end
---
+++ LomoPhotos/Photo.m
@@ -0,0 +1,14 @@
+//
+//  Photo.m
+//  LomoPhotos
+//
+//  Created by ben on 03.05.11.
+//  Copyright 2011 innomind. All rights reserved.
+//
+
+#import "Photo.h"
+#import "JSON.h"
+
+@implementation Photo
+
+@end
